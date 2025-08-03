34f50ee7c6efe36b72ac680095dd5057b7227057
sumu9897 <sumon.bncd@gmail.com>
2025-08-03 13:31:19 +0600

    all done

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,20 +1,22 @@
 import express from 'express'
+import {verifyToken} from '../middlewares/authMiddleware.js'
 
+import authorizeRoles from '../middlewares/roleMiddleware.js';
 const router = express.Router();
 
 // Only admin can accesss this routes
-router.get("/admin", (req,res)=>{
+router.get("/admin",verifyToken,authorizeRoles("admin"), (req,res)=>{
     res.json({message: "Welcome Admin"})
 })
 
 // Both admin and manager can accesss this routes
-router.get("/manager", (req,res)=>{
+router.get("/manager",verifyToken, authorizeRoles("admin", "manager"),(req,res)=>{
     res.json({message: "Welcome Manager"})
 })
 
 // All can accesss this routes
 
-router.get("/user", (req,res)=>{
+router.get("/user",verifyToken, authorizeRoles("admin", "manager","user"), (req,res)=>{
     res.json({message: "Welcome User"})
 })
 
